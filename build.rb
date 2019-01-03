93d805411427f1fac454c7c7fdedeaa9ef564f26
peitao <peitao@bytedance.com>
2019-01-03 20:37:19 +0800

    modify build.rb

--- build.rb
+++ build.rb
@@ -1,5 +1,5 @@
 p "make up cartfile"
 File.write("Cartfile", 'github "Masonry/Masonry"') 
 # 生成一个叫 Cartfile 的文件，让里面写入后面的字符串
-p "carthage update" `carthage update` # 安装编译
+p "carthage update" # 安装编译
 p "carthage update finish"
